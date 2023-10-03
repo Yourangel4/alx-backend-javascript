798f89f3623538dd922776cdd4b405c16dcbc522
yourangel4 <moyinoluwa20@gmail.com>
2023-10-03 18:22:25 +0000

    updated

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author moyinoluwa oluyeye <https://github.com/yourangel4>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
